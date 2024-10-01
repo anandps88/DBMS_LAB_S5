6228e387ea1600510836c7644fb45d677be7609b
milangmatt <milangeorgem@gmail.com>
2024-10-01 11:30:10 +0530

    break:exp10

--- PL_SQL/EXP6-BANKING/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE accounts (
-    acc_num NUMBER(5) NOT NULL PRIMARY KEY,
-    bal NUMBER(10)
-);
-
-INSERT INTO accounts values(1245,10000);
-INSERT INTO accounts values(6980,50000);
-INSERT INTO accounts values(7245,2000);
